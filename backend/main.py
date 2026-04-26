9ff362d0c9a7084e66baf696f945d66314811bc1
quarknihal <nihalkainth1@gmail.com>
2026-04-26 09:56:59 +0530

    url fixed

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://analyzer-mauve-six.vercel.app/"
+        "https://analyzer-mauve-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
