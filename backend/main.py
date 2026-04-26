70fb02beccd2bd66419299439597d4e18629baed
quarknihal <nihalkainth1@gmail.com>
2026-04-26 09:18:27 +0530

    connected frontend

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://analyzer-mauve-six.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
